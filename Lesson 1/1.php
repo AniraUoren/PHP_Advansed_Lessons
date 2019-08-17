0f59028c51f14847d8717343f8ec65d2264b1b52
AniraUoren <Airin6666@yandex.ru>
2019-08-17 19:02:06 +0300

    edit task 1 file

--- Lesson 1/1.php
+++ Lesson 1/1.php
@@ -1,13 +1,33 @@
 <?php
 
 class Product {
-    protected $id;
-    protected $vendorCode;
-    protected $name;
-    protected $description;
-    protected $price;
+    public $id;
+    public $vendorCode;
+    public $name;
+    public $description;
+    public $price;
+
+    public function __construct($id = 0, $vendorCode = 0, $name = 'nonename', $description = 'nodescription', $price = 0)
+    {
+        $this->id = $id;
+        $this->vendorCode = $vendorCode;
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+    }
+
+    public function DisplayProductCard(){
+        // вывод карточки товара на экран
+    }
+
+    public function EditProductParams(){
+        //метод для редактирования данных о товаре
+    }
+
+    public function DeleteProductFromDB(){
+        //удаление данных о товаре из базы данных
+    }
 
 }
 
-$product = new Product();
-var_dump($product);
+$product = new Product(1, 1000001, "t-shirt", "some description for t-shirt", 100);
