ce6bae0006fa1fc7e16fd8d114084c872a0e1e90
Anira Uoren <Airin6666@yandex.ru>
2019-10-30 14:17:15 +0300

    Edit task 1 classes.

--- Lesson 1/task_1.php
+++ Lesson 1/task_1.php
@@ -7,7 +7,7 @@ class Product {
     public $description;
     public $price;
 
-    public function __construct($id = 0, $vendorCode = 0, $name = 'nonename', $description = 'nodescription', $price = 0)
+    public function __construct($id, $vendorCode, $name, $description, $price)
     {
         $this->id = $id;
         $this->vendorCode = $vendorCode;
@@ -16,20 +16,54 @@ class Product {
         $this->price = $price;
     }
 
-    public function DisplayProductCard(){
+    public function displayProductCard(){
         // вывод карточки товара на экран
+
+        echo "<div>" .
+            "<p>". $this->vendorCode.  "</p>" .
+            "<p>". $this->name .  "</p>" .
+            "<p>". $this->description .  "</p>" .
+            "<p>" . $this->price ."</p>" .
+            "</div>";
     }
 
-    public function EditProductParams(){
+    public function editProductParams(){
         //метод для редактирования данных о товаре
     }
 
-    public function DeleteProductFromDB(){
+    public function deleteProductFromDB(){
         //удаление данных о товаре из базы данных
     }
 
 }
 
-$product = new Product(1, 1000001, "t-shirt", "some description for t-shirt", 100);
+class CartProduct extends Product{
+    public $quantity;
 
-//наследниками класса могут быть например карточки товара в корзине, т.к. там добавляется еще значение quantity
+    public function __construct($id, $vendorCode, $name, $description, $price, $quantity)
+    {
+        parent::__construct($id, $vendorCode, $name, $description, $price);
+        $this->quantity = $quantity;
+    }
+
+    public function displayProductCard()
+    {
+        echo "<div>" .
+            "<p>". $this->vendorCode.  "</p>" .
+            "<p>". $this->name .  "</p>" .
+            "<p>". $this->description .  "</p>" .
+            "<p>" . $this->price ."</p>" .
+            "<p>" . $this->quantity ."</p>" .
+            "</div>";
+    }
+}
+
+$product = new Product(1, 1000001, "t-shirt", "some description for t-shirt", 100);
+//$product->DisplayProductCard();
+//var_dump($product);
+$cartProduct = new CartProduct(2, 1000002, "t-shirt", "some description for t-shirt", 100, 3);
+
+echo "/////////////////////////Вывод карточки товара (каталог)////////////////";
+$product->displayProductCard();
+echo "/////////////////////////Вывод карточки товара (корзина)////////////////";
+$cartProduct->displayProductCard();
